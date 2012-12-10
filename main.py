ad072a1ec846df82649be79a591e8b55cf644d6c
CGearhart <cgearhart@gmail.com>
2012-12-10 00:20:00 -0500

    Refactored HTML & CSS to be more compliant with google style guides. Began implementing user authentication.

--- main.py
+++ main.py
@@ -14,12 +14,117 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+from collections import namedtuple
+
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')    # __file__ is *this* file
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+
+### MODELS ###
+
+class BlogPosts(db.Model):
+    """Create datastore with subject, content, and created date."""
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class Users(db.Model):
+    username = db.StringProperty(required = True)
+    password = db.StringProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+
+### HELPER CLASSES ###
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        """Helper function for render()"""
+        self.response.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        """Helper function for render()"""
+        jinja_template = jinja_env.get_template(template)
+        return jinja_template.render(params)
+
+    def render(self, template, **kw):
+        """Helper function combining self.write and self.redner_str that is exposed to dependent classes"""
+        self.write(self.render_str(template,**kw))
+
+class User_validation():
+    # TODO(CG): implement hashing, username checking, etc.
+    pass
+
+
+### PAGE HANDLERS ###
+
+class BaseRedirect(webapp2.RequestHandler):
+    """Redirect to blog homepage"""
     def get(self):
-        self.response.write('Hello world!')
+        self.redirect('/blog')
+
+
+class MainPage(Handler):
+    """Basic landing page"""
+    def get(self):
+        posts_from_db = db.GqlQuery("SELECT * FROM BlogPosts ORDER BY created DESC")
+        self.render("main.html",posts=posts_from_db)
+
+
+class NewPost(Handler):
+    """Add a new post to the blog"""
+    def get(self,**params):
+        self.render("form.html",**params)
+    
+    def post(self):
+        entry_data = {}
+        entry_data['subject'] = self.request.get('subject')
+        entry_data['content'] = self.request.get('content')
+        
+
+        if entry_data['subject'] and entry_data['content']:
+            bp = BlogPosts(**entry_data)
+            post_key = bp.put()    # Write the values to the model
+        
+            self.redirect('/blog/%d' % int(post_key.id()))
+            
+        else:
+            entry_data['error'] = "You need both a valid subject and content."
+            self.get(**entry_data)    # redirect to the same page and re-render with error messages
+
+
+class Permalink(Handler):
+    def get(self,entry_id):
+        key = db.Key.from_path('BlogPosts', int(entry_id))    # Look for a post by entry_id
+        bp = db.get(key)
+        
+        if not bp:
+            self.redirect('/404')    # Kludge way to use the built-in 404 handler (for consistency)
+            return
+        
+        self.render("main.html", posts=[bp], menu="home")
+
+
+class Signup(Handler):
+    def get(self):
+        self.render("signup.html",error={})
+
+    def post(self):
+        # TODO(CG): implement username registration, error checking, login, hashing, etc.
+        pass
+        
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+    ('/', BaseRedirect),
+    ('/blog', MainPage),
+    ('/blog/newpost', NewPost),
+    ('/blog/(\d+)', Permalink),
+    ('/blog/signup', Signup)
+   ], debug=True)
