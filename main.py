8d5a67e986e08c69b9f431fb9dbef22140913291
CGearhart <cgearhart@gmail.com>
2013-01-17 01:38:27 -0500

    signup feature functioning.

--- main.py
+++ main.py
@@ -17,6 +17,10 @@
 import os
 import webapp2
 import jinja2
+import string
+import random
+import hashlib
+import re
 
 from google.appengine.ext import db
 from collections import namedtuple
@@ -37,13 +41,50 @@ class BlogPosts(db.Model):
 
 class Users(db.Model):
     username = db.StringProperty(required = True)
-    password = db.StringProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
+    salt = db.StringProperty(required = True)
+    pw_hash = db.StringProperty(required = True)
+
+
+### HELPER FUNCTIONS ###
+
+user_re = re.compile("^[a-zA-Z0-9_-]{3,20}$")
+pass_re = re.compile("^.{3,20}$")
+email_re = re.compile("^[\S]+@[\S]+\.[\S]+$")
+
+def valid_username(username):
+    return user_re.match(username)
+
+def valid_password(password):
+    return pass_re.match(password)
+
+def valid_email(email):
+    return email_re.match(email)
+
+def make_salt():
+    """Generate a random 5-character string to use as a password salt"""
+    salt = ''
+    for i in xrange(5):
+        salt += random.choice(string.ascii_letters)
+    return salt
+
+def make_pw_hash(name, pw, salt=None):
+    """Use sha256 hash function to create or validate a username/password hash combination"""
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return (salt, h)
+
+def valid_pw(name, pw, h):
+    """Validate a username/password combination"""
+    hash,salt = h.split(',')
+    if make_pw_hash(name,pw,salt) == h:
+        return True
 
 
 ### HELPER CLASSES ###
 
 class Handler(webapp2.RequestHandler):
+
     def write(self, *a, **kw):
         """Helper function for render()"""
         self.response.write(*a, **kw)
@@ -56,10 +97,6 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         """Helper function combining self.write and self.redner_str that is exposed to dependent classes"""
         self.write(self.render_str(template,**kw))
-
-class User_validation():
-    # TODO(CG): implement hashing, username checking, etc.
-    pass
 
 
 ### PAGE HANDLERS ###
@@ -116,9 +153,49 @@ class Signup(Handler):
         self.render("signup.html",error={})
 
     def post(self):
-        # TODO(CG): implement username registration, error checking, login, hashing, etc.
-        pass
-        
+        error = {}
+        username = self.request.get('username')
+        password = self.request.get('password')
+        password2 = self.request.get('verify')
+        email = self.request.get('email')
+
+        user_from_db = db.GqlQuery("SELECT * FROM Users where username=:1", username).get()
+
+        if not username or not valid_username(username): # username is required and must be valid
+            error['username'] = 'Invalid username.'
+        elif user_from_db and username == user_from_db.username: # user should not exist in the db
+            error['username'] = 'User already exists.'
+        elif not password or not valid_password(password): # password is required and must be valid
+            error['password'] = 'Invalid password.'
+        elif not password2 or password != password2: # make sure password matches
+            error['verify'] = 'Passwords do not match.'
+        elif email and not valid_email(email): # validate email if given
+            error['email'] = 'Invalid email address.'
+
+        if error:
+            self.render("signup.html",error=error)
+        else:
+            salt,h = make_pw_hash(username,password)
+            user = Users(username = username,
+                         pw_hash = h,
+                         salt = salt)
+            user.put()
+            user_id = user.key().id()
+            self.response.headers.add_header('Set-Cookie', 'user_id=%d|%s' % (user_id,h))
+            self.redirect('/blog/welcome')
+
+
+class Welcome(Handler):
+    def get(self):
+        user_cookie = self.request.cookies.get('user_id', None)
+        if not user_cookie:
+            self.redirect('/blog/signup')
+        cookie_user_id,cookie_h = tuple(user_cookie.split('|'))
+        user = Users.get_by_id(int(cookie_user_id))
+        if not cookie_h == user.pw_hash:
+            self.redirect('/blog/signup')
+        else:
+            self.render("welcome.html",username=user.username)
 
 
 app = webapp2.WSGIApplication([
@@ -126,5 +203,6 @@ app = webapp2.WSGIApplication([
     ('/blog', MainPage),
     ('/blog/newpost', NewPost),
     ('/blog/(\d+)', Permalink),
-    ('/blog/signup', Signup)
+    ('/blog/signup', Signup),
+    ('/blog/welcome', Welcome)
    ], debug=True)
