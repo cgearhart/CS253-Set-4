01baf7ef97de69c008b29fbe647796d4145a314e
CGearhart <cgearhart@gmail.com>
2013-01-18 09:54:13 -0500

    Logout was incorrectly redirecting to login instead of signup.

--- main.py
+++ main.py
@@ -219,7 +219,7 @@ class Login(Handler):
 class Logout(Handler):
     def get(self):
         self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
-        self.redirect('/blog/login')
+        self.redirect('/blog/signup')
 
 
 class Welcome(Handler):
